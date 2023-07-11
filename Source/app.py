4ccac5582c74aa9077c0537f96f992a59109333f
chanakya pendem <nani30112000@gmail.com>
2023-07-11 18:27:35 +0530

    new

--- Source/app.py
+++ Source/app.py
@@ -1,8 +1,26 @@
-from flask import Flask, render_template
+import datetime
+from markupsafe import escape
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
+@app.route('/add/<int:n1>/<int:n2>/')
+def add(n1,n2):
+    return '<h1>{}</h2>'.format(n1+n2)
+@app.route('/users/<int:user_id>/')
+def greet(user_id):
+    users = ['Ashley', 'Nelson' , 'Harry']
+    try:
+        return '<h3>Hi {}</h3>'.format(users[user_id])
+    except IndexError:
+        abort(404)
